68eabc1b5e8e9396f527362776830d81c4e92998
Bruno Catani <brunohcatani@gmail.com>
2022-10-17 22:04:41 -0300

    Aula 13/10/2022

---
+++ site-react/src/App.jsx
@@ -0,0 +1,26 @@
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import { HomePage } from "./pages/Home";
+
+
+function App() {
+
+  return (
+    <div className="App">
+
+      <Header />
+
+      <main>
+        <Routes>
+          <Route path="*" element={<HomePage />} />
+        </Routes>
+
+      </main>
+
+      <Footer />
+
+    </div >
+  )
+}
+
+export default App
