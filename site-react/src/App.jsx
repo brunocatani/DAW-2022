61e07c74a9b23e70c1088e9480c055faf84f456b
Bruno Catani <brunohcatani@gmail.com>
2022-10-20 22:08:59 -0300

    Aula 20/10/2022

--- site-react/src/App.jsx
+++ site-react/src/App.jsx
@@ -1,7 +1,9 @@
+import {Route, Routes} from "react-router-dom"
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { HomePage } from "./pages/Home";
-
+import './assets/css/normalize.css'
+import './assets/css/global.css'
 
 function App() {
 
