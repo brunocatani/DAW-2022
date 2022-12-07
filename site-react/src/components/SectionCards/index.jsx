f8eceeec8f1aa1fc3b6cb73d823a5a3f25f415cc
Bruno Catani <brunohcatani@gmail.com>
2022-12-07 20:02:49 -0300

    Aula 17/11/2022

--- site-react/src/components/SectionCards/index.jsx
+++ site-react/src/components/SectionCards/index.jsx
@@ -1,5 +1,6 @@
+import { IntlProvider, FormattedNumber } from 'react-intl';
 import { useImovel } from '../../hook/useImovel';
-import {IntlProvider, FormattedNumber} from 'react-intl'
+
 import './style.css';
 
 
@@ -11,29 +12,35 @@ export function SectionCards() {
         <section id="cards">
             <h2>Imóveis disponíveis</h2>
 
-            <div className="cards-content">
+            {imoveis.length > 0 ? (
+                <div className="cards-content">
 
-                {imoveis.map((item, index) => (
-                    <div key={index} className="card">
-                        <img src={item.foto} alt="Casa 1" />
-                        <div className="card-content">
-                            <h4>{ item.titulo }</h4>
-                            <p>{ item.descricao }</p>
+                    {imoveis.map((item, index) => (
+                        <div key={index} className="card">
+                            <img src={item.foto} alt="Casa 1" />
+                            <div className="card-content">
+                                <h4>{item.titulo}</h4>
+                                <p>{item.descricao}</p>
 
-                            <div className="card-price">
-                                <p>
-                                    <IntlProvider Locale="pt-BR">
-                                        <FormattedNumber value={item.preco}
-                                            style="currency"
-                                            currency='brl' />
-                                    </IntlProvider>
-                                </p>
+                                <div className="card-price">
+                                    <p>
+                                        <IntlProvider locale="pt-BR">
+                                            <FormattedNumber value={item.preco}
+                                                style="currency"
+                                                currency="BRL" />
+                                        </IntlProvider>
+                                    </p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
 
-            </div>
+                </div>
+            ) : (
+                <h3 className="not-found">Nenhum imóvel foi encontrado!</h3>
+            ) }
+
+
         </section>
     )
 }
